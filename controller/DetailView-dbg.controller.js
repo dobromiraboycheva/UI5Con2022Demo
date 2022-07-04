274c52884b4a05c58bca4cc08a4433ebc1a76833
Okiana <evdokia.yordanova@sap.com>
2022-07-04 06:28:32 +0000

    Deploying to gh-pages from @ dobromiraboycheva/UI5Con2022Demo@60f2d990d811ac2596d95becd6763ceccb7112b8 🚀

--- controller/DetailView-dbg.controller.js
+++ controller/DetailView-dbg.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(
-  ["./BaseController"],
+  ["./BaseController",
+  "sap/m/MessageToast"],
   /**
    * @param {typeof sap.ui.core.mvc.Controller} Controller
    */
-  function (Controller) {
+  function (Controller, MessageToast) {
     "use strict";
 
     return Controller.extend("smart.controls.demo.controller.DetailView", {
@@ -14,9 +15,32 @@ sap.ui.define(
 
       _onObjectMatched: function (oEvent) {
         const sPath = `/${window.decodeURIComponent(oEvent.getParameter("arguments").detailPath)}`,
-          oView = this.getView();
+        oView = this.getView();
         oView.bindElement(sPath);
       },
+      handleEditToggled: function (sSmartFormId) {
+        var oSmartForm = this.getView().byId(sSmartFormId),
+        oButton = this.getView().byId("button");
+
+        if (oSmartForm.getEditable()) {
+          oButton.setVisible(true);
+        } else {
+            oButton.setVisible(false);
+        }
+      },
+      handleEditToggledEurope: function () {
+        this.handleEditToggled("smartFormEurope");
+      },
+      handleEditToggledAmerica: function () {
+        this.handleEditToggled("smartFormAmerica");
+      },
+      handleEditToggledAsia: function () {
+        this.handleEditToggled("smartFormAsia");
+      },
+      handleSaveButtonPress:  function () {
+        var sText = "Saved";
+        MessageToast.show(sText);
+      }
     });
   }
 );
