ee392d1bf65ec4fefde8dedbf5ab685906f74f1b
Okiana <evdokia.yordanova@sap.com>
2022-07-06 08:41:25 +0000

    Deploying to gh-pages from @ dobromiraboycheva/UI5Con2022Demo@ca66811deb3005490860ba0101ea4aa67a5e8598 🚀

--- resources/sap/ui/Global-dbg.js
+++ resources/sap/ui/Global-dbg.js
@@ -77,7 +77,7 @@ sap.ui.define([
 		 */
 		version: "1.102.0",
 		// buildinfo.lastchange is deprecated and is therefore defaulted to empty string
-		buildinfo : { lastchange : "", buildtime : "20220705-1446" }
+		buildinfo : { lastchange : "", buildtime : "20220706-0838" }
 	});
 
 	var oCfgData = window["sap-ui-config"] || {};
